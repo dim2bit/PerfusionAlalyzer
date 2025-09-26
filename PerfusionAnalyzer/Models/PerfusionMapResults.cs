6fd83510e587d97582cdb14d19cdf003c0be1a52
dim2bit <dimalobunko@ukr.net>
2025-09-26 21:46:50 +0300

    removed old files

--- PerfusionAnalyzer/Models/PerfusionMapResults.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PerfusionAnalyzer.Models;
-
-public class PerfusionMapResults
-{
-    public float[,] AucMap { get; set; }
-    public float[,] MttMap { get; set; }
-    public float[,] TtpMap { get; set; }
-}
